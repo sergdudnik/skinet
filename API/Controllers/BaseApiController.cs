0b709b8676aec34cb267d18e9aab49667bfe0c0d
Sergei Dudnik <sergei.dudnik@ut.ee>
2021-07-15 18:37:27 +0300

    End of Part 5

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("API/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
